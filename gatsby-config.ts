bb90aa78f3a28509ecbe2e7f4fe326120ac4e28c
Warpfox <tylermwatts@gmail.com>
2023-04-15 11:04:05 -0500

    Init commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,29 +1,43 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Cybermoms`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `Cybermoms`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		'gatsby-plugin-image',
+		'gatsby-transformer-remark',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+		{
+			resolve: 'gatsby-source-contentful',
+			options: {
+				spaceId: process.env.SPACE_ID,
+				accessToken: process.env.CONTENT_DELIVERY_API_TOKEN,
+			},
+		},
+	],
 };
 
 export default config;
